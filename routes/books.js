cf96d3ac354e8b1fc7078614bf2e158fc282ea56
sanjay9 <sanjaymahabir9@gmail.com>
2023-03-27 10:55:30 -0400

    Updates to Registration and Bookupload
    Fixed AddBook only appears when logged in
    Added Date and view fields to book model to gauge popularity
    New user Registration does not allow accounts with same email to created
    Book upload now handles an image upload

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,49 @@ const express = require("express");
 const router = express.Router();
 const Book = require("../models/book.js")
 
+//https://www.sammeechward.com/uploading-images-express-and-react#react
+//Used for image uploads to store images in a folder (on the server)
+// 1
+const multer = require('multer')
+// 2
+const upload = multer({ dest: './BookImagesUploaded/' })
+
 //Creating one book
+router.post("/upload/", upload.single('image'), async (req, res) => {
+  console.log("Server made it")
+  let fileName = ''
+  if (req.file) {
+    fileName = req.file.filename
+  }
+  
+
+  const book = new Book({  
+    
+    title: req.body.title,
+    isbn: req.body.isbn,
+    authors: req.body.authors,
+    genre: req.body.genre,
+    price: req.body.price,
+    description: req.body.description,
+    sellerEmail: req.body.sellerEmail,
+    image: fileName 
+
+  }); 
+  console.log(book)  
+  try {
+    const newBook = await book.save();
+    //res.send(newBook)
+    //res.send('success')
+    res.status(201).json(newBook);
+  } catch (err) {
+    //res.send("ERROR: " + err.message)
+    res.status(400).json({ message: err.message });
+  }
+
+});
+
+
+/*
 router.post("/upload/", async (req, res) => {
   const book = new Book({  
     
@@ -23,6 +65,7 @@ router.post("/upload/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+*/
 
 router.delete("/:isbn", getBookByIsbn, async (req, res) => {
   try {
